8b748370b46c5aad814e3a9e71a0a42ee4b535d4
David Vreony <dpvreony@users.noreply.github.com>
2016-03-17 00:08:21 +0000

    rename of unit test project

---
+++ src/Dhgms.Nucleotide.UnitTests/BaseUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dhgms.Nucleotide.Tests
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using Xunit.Abstractions;
+
+    [ExcludeFromCodeCoverage]
+    public class BaseUnitTests
+    {
+        public BaseUnitTests(ITestOutputHelper output)
+        {
+            if (output == null)
+            {
+                throw new ArgumentNullException("output");
+            }
+
+            this.OutputHelper = output;
+        }
+
+        protected ITestOutputHelper OutputHelper { get; private set; }
+    }
+}
