ec5507d6506b4216a2942bf95cf0125fcee3a001
David Vreony <dpvreony@users.noreply.github.com>
2017-01-08 09:09:35 +0000

    renamed generators

---
+++ src/Dhgms.Nucleotide/Generators/InformationClassUnitTestGenerator.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UnitTestInformation.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Dhgms.Nucleotide.Generators
+{
+    using System.Text;
+
+    using Dhgms.Nucleotide.PropertyInfo;
+
+    /// <summary>
+    /// Unit Test Generator for an Information Class
+    /// </summary>
+    public sealed class UnitTestInformation : UnitTestBase
+    {
+        /// <summary>
+        /// Generate the unit test code for the constructor that takes parameters
+        /// </summary>
+        /// <param name="sb">string build to append the code to</param>
+        /// <param name="className">name of the class</param>
+        /// <param name="properties">collection of properties</param>
+        /// <param name="baseClassName">name of the base class</param>
+        /// <param name="baseClassProperties">collection of base class properties</param>
+        protected override void DoConstructorWithParameters(
+            StringBuilder sb,
+            string className,
+            PropertyInfoBase[] properties,
+            string baseClassName,
+            PropertyInfoBase[] baseClassProperties)
+        {
+        }
+
+        /// <summary>
+        ///     Gets the class suffix
+        /// </summary>
+        /// <returns>
+        ///     the class suffix
+        /// </returns>
+        protected override string GetClassSuffix()
+        {
+            return null;
+        }
+    }
+}
