c21cdb3f3e705754e9422d761c6eff9db9f435a0
David Vreony <dpvreony@users.noreply.github.com>
2017-01-08 23:46:41 +0000

    update of unit test name

--- src/Dhgms.Nucleotide.UnitTests/Generators/QueryableRepositoryIntefaceGeneratorTests.cs
+++ src/Dhgms.Nucleotide.UnitTests/Generators/QueryableRepositoryIntefaceGeneratorTests.cs
@@ -2,11 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Dhgms.Nucleotide.Generators;
+using Microsoft.CodeAnalysis;
+using Moq;
+using Xunit;
 
 namespace Dhgms.Nucleotide.UnitTests.Generators
 {
-    class RepositoryGeneratorTests
+    public static class QueryableRepositoryIntefaceGeneratorTests
     {
+        public sealed class ConstructorMethod
+        {
+            [Fact]
+            public void ReturnsInstance()
+            {
+                var attributeData = new Mock<AttributeData>();
+                var instance = new QueryableRepositoryIntefaceGenerator(attributeData.Object);
+                Assert.NotNull(instance);
+            }
+        }
+
+        public sealed class GenerateAsyncMethod
+        {
+            [Fact]
+            public async Task ReturnsInstance()
+            {
+                var attributeData = new Mock<AttributeData>();
+                var instance = new QueryableRepositoryIntefaceGenerator(attributeData.Object);
+
+                var result = await instance.GenerateAsync(null, null, null, CancellationToken.None);
+                Assert.NotNull(result);
+            }
+        }
     }
 }
