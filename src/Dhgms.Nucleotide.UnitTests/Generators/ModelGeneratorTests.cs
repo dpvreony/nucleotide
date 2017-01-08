4ed473ecf610b1bfeb4429e49058b2880dedbd68
David Vreony <dpvreony@users.noreply.github.com>
2017-01-08 23:37:09 +0000

    stubbed unit tests for generators

---
+++ src/Dhgms.Nucleotide.UnitTests/Generators/ModelGeneratorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Dhgms.Nucleotide.Generators;
+using Microsoft.CodeAnalysis;
+using Moq;
+using Xunit;
+
+namespace Dhgms.Nucleotide.UnitTests.Generators
+{
+    public static class ModelGeneratorTests
+    {
+        public sealed class ConstructorMethod
+        {
+            [Fact]
+            public void ReturnsInstance()
+            {
+                var attributeData = new Mock<AttributeData>();
+                var instance = new ModelGenerator(attributeData.Object);
+                Assert.NotNull(instance);
+            }
+        }
+
+        public sealed class GenerateAsyncMethod
+        {
+            [Fact]
+            public async Task ReturnsInstance()
+            {
+                var attributeData = new Mock<AttributeData>();
+                var instance = new ModelGenerator(attributeData.Object);
+
+                var result = await instance.GenerateAsync(null, null, null, CancellationToken.None);
+                Assert.NotNull(result);
+            }
+        }
+    }
+}
