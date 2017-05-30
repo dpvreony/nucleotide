df9fb02af3686dfba511ed6af32d6178c3ca1445
David Vreony <dpvreony@users.noreply.github.com>
2017-05-31 00:29:20 +0100

    xunit netcore prep work

---
+++ src/Dhgms.Nucleotide.UnitTests/Generators/BaseGeneratorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dhgms.Nucleotide.Generators;
+using Microsoft.CodeAnalysis;
+using Xunit;
+using System.Diagnostics.CodeAnalysis;
+using CodeGeneration.Roslyn;
+
+namespace Dhgms.Nucleotide.UnitTests.Generators
+{
+    //[ExcludeFromCodeCoverage]
+    public static class GeneratorTests
+    {
+        private static IEnumerable<object[]> TestData = new[]
+        {
+            CommandClassGeneratorTestData
+        };
+
+        private static CodeGeneratorFactory CommandClassGeneratorFactory =
+            attributeData => new CommandClassGenerator(attributeData);
+
+        private static object[] CommandClassGeneratorTestData = new object[]
+        {
+            CommandClassGeneratorFactory
+        };
+
+        public delegate ICodeGenerator CodeGeneratorFactory(AttributeData attributeData);
+
+        public sealed class ConstructorMethod
+        {
+            [Theory]
+            [MemberData(nameof(TestData))]
+            public void ThrowsArgumentNullException(CodeGeneratorFactory factory)
+            {
+            }
+
+        }
+    }
+}
