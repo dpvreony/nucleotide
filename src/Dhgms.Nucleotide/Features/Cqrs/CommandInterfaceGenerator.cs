e2287d79a010a73372b665ba82e026d80d01887f
David Vreony <dpvreony@users.noreply.github.com>
2018-04-15 13:01:59 +0100

    command interfaces base ref

--- src/Dhgms.Nucleotide/Features/Cqrs/CommandInterfaceGenerator.cs
+++ src/Dhgms.Nucleotide/Features/Cqrs/CommandInterfaceGenerator.cs
@@ -11,176 +11,62 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Dhgms.Nucleotide.Features.Cqrs
 {
-    public sealed class CommandInterfaceGenerator : BaseGenerator
+    public sealed class CommandInterfaceGenerator : BaseInterfaceLevelCodeGenerator
     {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueryInterfaceGenerator"/> class. 
+        /// </summary>
         public CommandInterfaceGenerator(AttributeData attributeData) : base(attributeData)
         {
         }
 
-        protected override string[] GetClassPrefixes() => null;
+        protected override string[] GetClassPrefixes() => new []{ "Add", "Delete", "Update" };
+
+        protected override string[] GetInterfaceSummary(IEntityGenerationModel entityDeclaration)
+        {
+            return new[]
+            {
+                $"Command for {entityDeclaration.ClassName}"
+            };
+        }
 
         protected override string GetClassSuffix() => "Command";
 
         protected override string GetNamespace() => "Commands";
 
-        protected override PropertyDeclarationSyntax GetPropertyDeclaration(PropertyInfoBase propertyInfo)
+        protected override PropertyDeclarationSyntax[] GetPropertyDeclarations(
+            IEntityGenerationModel entityGenerationModel, string prefix)
         {
             return null;
         }
 
-        protected override PropertyDeclarationSyntax GetReadOnlyPropertyDeclaration(PropertyInfoBase propertyInfo)
+
+        protected override MethodDeclarationSyntax[] GetMethodDeclarations(string className, string prefix)
         {
             return null;
         }
 
-        protected override PropertyDeclarationSyntax GetPropertyDeclaration(PropertyInfoBase propertyInfo,
-            AccessorDeclarationSyntax[] accessorList, IEnumerable<SyntaxTrivia> summary)
+        protected override string[] GetBaseInterfaces(IEntityGenerationModel entityGenerationModel, string prefix)
         {
-            return null;
-        }
-
-        protected override async Task<NamespaceDeclarationSyntax> GenerateObjects(NamespaceDeclarationSyntax namespaceDeclaration, EntityGenerationModel[] generationModelEntityGenerationModel)
-        {
-            if (generationModelEntityGenerationModel == null || generationModelEntityGenerationModel.Length < 1)
+            // this is a temporary workaround
+            // this class needs splitting into 2 levels
+            // there is a namespace \ interface collection level generator
+            // then a class\interface level generator
+            // the prefixes thing needs to go, it's flawed
+            // the collection level should create instances of a reusable interface level generator.
+            if (prefix.Equals("Delete"))
             {
-                namespaceDeclaration = namespaceDeclaration.WithLeadingTrivia(SyntaxFactory.Comment("#error DROP OUT 2"));
-                return namespaceDeclaration;
-            }
-
-            var classDeclarations = new List<MemberDeclarationSyntax>();
-
-            var prefixes = GetClassPrefixes();
-            if (prefixes == null || prefixes.Length == 0)
-            {
-                prefixes = new[] { string.Empty };
-            }
-
-            var suffix = GetClassSuffix();
-            foreach (var generationModelClassGenerationParameter in generationModelEntityGenerationModel)
-            {
-                foreach (var prefix in prefixes)
+                return new[]
                 {
-                    classDeclarations.Add(await GetAddInterfaceDeclarationSyntax(generationModelClassGenerationParameter, prefix, suffix));
-                    classDeclarations.Add(await GetDeleteInterfaceDeclarationSyntax(generationModelClassGenerationParameter, prefix, suffix));
-                    classDeclarations.Add(await GetUpdateInterfaceDeclarationSyntax(generationModelClassGenerationParameter, prefix, suffix));
-                }
+                    $"Dhgms.AspNetCoreContrib.Abstractions.IAuditableRequest<long, ResponseDtos.{prefix}{entityGenerationModel.ClassName}ResponseDto>"
+                };
             }
 
-            return await Task.FromResult(namespaceDeclaration.AddMembers(classDeclarations.ToArray()));
-        }
-
-        protected string[] GetInterfaceSummary(IEntityGenerationModel entityDeclaration, string prefix)
-        {
             return new[]
             {
-                $"{prefix} Command for {entityDeclaration.ClassName}"
+                $"Dhgms.AspNetCoreContrib.Abstractions.IAuditableRequest<RequestDtos.{prefix}{entityGenerationModel.ClassName}RequestDto, ResponseDtos.{prefix}{entityGenerationModel.ClassName}ResponseDto>"
             };
-        }
-
-        protected PropertyDeclarationSyntax[] GetPropertyDeclarations(IEntityGenerationModel entityGenerationModel)
-        {
-            return null;
-        }
-
-        //protected MethodDeclarationSyntax[] GetMethodDeclarations(string entityName)
-        //{
-        //    return new[]
-        //    {
-        //        GetExecuteMethodDeclaration(entityName)
-        //    };
-        //}
-
-        //protected MethodDeclarationSyntax GetExecuteMethodDeclaration(string entityName)
-        //{
-        //    var methodName = "ExecuteAsync";
-
-        //    var parameters = GetParams(new[] { GetExecuteRequestParameter(entityName), "System.Security.Claims.ClaimsPrincipal claimsPrincipal" });
-        //    var returnType = SyntaxFactory.ParseTypeName("System.Threading.Tasks.Task<string>");
-        //    var declaration = SyntaxFactory
-        //        .MethodDeclaration(returnType, methodName)
-        //        .WithParameterList(parameters)
-        //        .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken));
-        //    return declaration;
-        //}
-
-        private async Task<MemberDeclarationSyntax> GetAddInterfaceDeclarationSyntax(IEntityGenerationModel entityDeclaration, string prefix, string suffix)
-        {
-            var className = $"IAdd{entityDeclaration.ClassName}{suffix}";
-            var members = GetMembers(entityDeclaration);
-            var leadingTrivia = GetInterfaceLeadingTrivia(entityDeclaration, prefix);
-            var declaration = SyntaxFactory.InterfaceDeclaration(className)
-                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-                .AddMembers(members)
-                .WithLeadingTrivia(leadingTrivia);
-
-            //var baseInterfaces = GetBaseInterfaces(entityDeclaration);
-            //if (baseInterfaces != null && baseInterfaces.Length > 0)
-            //{
-            //    var b = baseInterfaces.Select(bi => SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName(bi)) as BaseTypeSyntax).ToArray();
-            //    declaration = declaration.AddBaseListTypes(b);
-            //}
-
-            return await Task.FromResult(declaration);
-        }
-
-        private async Task<MemberDeclarationSyntax> GetDeleteInterfaceDeclarationSyntax(IEntityGenerationModel entityDeclaration, string prefix, string suffix)
-        {
-            var className = $"IDelete{entityDeclaration.ClassName}{suffix}";
-            var members = GetMembers(entityDeclaration);
-            var leadingTrivia = GetInterfaceLeadingTrivia(entityDeclaration, prefix);
-            var declaration = SyntaxFactory.InterfaceDeclaration(className)
-                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-                .AddMembers(members)
-                .WithLeadingTrivia(leadingTrivia);
-
-            //var baseInterfaces = GetBaseInterfaces(entityDeclaration);
-            //if (baseInterfaces != null && baseInterfaces.Length > 0)
-            //{
-            //    var b = baseInterfaces.Select(bi => SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName(bi)) as BaseTypeSyntax).ToArray();
-            //    declaration = declaration.AddBaseListTypes(b);
-            //}
-
-            return await Task.FromResult(declaration);
-        }
-
-        private async Task<MemberDeclarationSyntax> GetUpdateInterfaceDeclarationSyntax(IEntityGenerationModel entityDeclaration, string prefix, string suffix)
-        {
-            var className = $"IUpdate{entityDeclaration.ClassName}{suffix}";
-            var members = GetMembers(entityDeclaration);
-            var leadingTrivia = GetInterfaceLeadingTrivia(entityDeclaration, prefix);
-            var declaration = SyntaxFactory.InterfaceDeclaration(className)
-                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-                .AddMembers(members)
-                .WithLeadingTrivia(leadingTrivia);
-
-            //var baseInterfaces = GetBaseInterfaces(entityDeclaration);
-            //if (baseInterfaces != null && baseInterfaces.Length > 0)
-            //{
-            //    var b = baseInterfaces.Select(bi => SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName(bi)) as BaseTypeSyntax).ToArray();
-            //    declaration = declaration.AddBaseListTypes(b);
-            //}
-
-            return await Task.FromResult(declaration);
-        }
-
-        protected MemberDeclarationSyntax[] GetMembers(IEntityGenerationModel entityGenerationModel)
-        {
-            var result = new List<MemberDeclarationSyntax>();
-
-            //var properties = GetPropertyDeclarations(entityGenerationModel);
-            //AddToList(result, properties);
-
-            //var methods = GetMethodDeclarations(entityGenerationModel.ClassName);
-            //AddToList(result, methods);
-
-            return result.ToArray();
-        }
-
-        private IEnumerable<SyntaxTrivia> GetInterfaceLeadingTrivia(IEntityGenerationModel entityDeclaration, string prefix)
-        {
-            var interfaceSummary = GetInterfaceSummary(entityDeclaration, prefix);
-
-            return GetSummary(interfaceSummary);
         }
     }
 }
