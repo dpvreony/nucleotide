805c57cd79f464dc534e1080b721c673127ab3d6
David Vreony <dpvreony@users.noreply.github.com>
2020-10-15 23:45:16 +0100

    *BREAKING* split CGR loader and generator
    making generators easier to test

---
+++ src/Dhgms.Nucleotide.Generators/GeneratorProcessors/BaseGeneratorProcessor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dhgms.Nucleotide.Model;
+using Dhgms.Nucleotide.PropertyInfo;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Dhgms.Nucleotide.Generators.GeneratorProcessors
+{
+    public abstract class BaseGeneratorProcessor
+    {
+        public abstract Task<NamespaceDeclarationSyntax> GenerateObjects(
+            NamespaceDeclarationSyntax namespaceDeclaration,
+            INucleotideGenerationModel nucleotideGenerationModel);
+
+        protected abstract string[] GetClassPrefixes();
+
+        protected abstract PropertyDeclarationSyntax GetPropertyDeclaration(PropertyInfoBase propertyInfo);
+
+        protected abstract PropertyDeclarationSyntax GetReadOnlyPropertyDeclaration(PropertyInfoBase propertyInfo);
+
+        protected IEnumerable<SyntaxTrivia> GetSummary(string[] summaryLines)
+        {
+            var result = new List<SyntaxTrivia>
+            {
+                SyntaxFactory.Comment($"/// <summary>")
+            };
+
+            var lines = summaryLines.Select(line => SyntaxFactory.Comment($"/// {line}"));
+            result.AddRange(lines);
+
+            result.Add(SyntaxFactory.Comment($"/// </summary>"));
+
+            return result;
+        }
+
+        protected abstract PropertyDeclarationSyntax GetPropertyDeclaration(
+            PropertyInfoBase propertyInfo,
+            AccessorDeclarationSyntax[] accessorList,
+            IEnumerable<SyntaxTrivia> summary);
+
+        protected static void AddToList<T>(List<MemberDeclarationSyntax> list, IReadOnlyCollection<T> items)
+            where T : MemberDeclarationSyntax
+        {
+            if (items != null && items.Count > 0)
+            {
+                list.AddRange(items);
+            }
+        }
+
+        protected static ParameterListSyntax GetParams(string[] argCollection)
+        {
+            var parameters = SyntaxFactory.SeparatedList<ParameterSyntax>();
+
+            foreach (var s in argCollection)
+            {
+                var node = SyntaxFactory.Parameter(SyntaxFactory.Identifier(s));
+                parameters = parameters.Add(node);
+            }
+
+            return SyntaxFactory.ParameterList(parameters);
+        }
+    }
+}
