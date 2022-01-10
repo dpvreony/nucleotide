0f98e67d835983abc25e2e973f74a7b684c16751
David Vreony <dpvreony@users.noreply.github.com>
2022-01-10 21:27:41 +0000

    add unit tests for crud controller log message action generator

---
+++ src/Dhgms.Nucleotide.UnitTests/Generators/Features/Logging/CrudControllerLoggerMessageActionGeneratorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Dhgms.Nucleotide.Generators.Features.Logging;
+using Dhgms.Nucleotide.Generators.Models;
+using Dhgms.Nucleotide.SampleGenerator;
+using Microsoft.CodeAnalysis;
+using Xunit.Abstractions;
+
+namespace Dhgms.Nucleotide.UnitTests.Generators.Features.Logging
+{
+    [ExcludeFromCodeCoverage]
+    public static class CrudControllerLoggerMessageActionGeneratorTests
+    {
+        public sealed class ConstructorMethod : BaseGeneratorTests.BaseConstructorMethod<CrudControllerLoggerMessageActionGenerator, CrudControllerLoggerMessageActionFeatureFlags, CrudControllerLoggerMessageActionGeneratorProcessor, IEntityGenerationModel>
+        {
+            public ConstructorMethod(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            protected override Func<AttributeData, CrudControllerLoggerMessageActionGenerator> GetFactory()
+            {
+                return data => new TestCrudControllerLoggerMessageActionGenerator();
+            }
+        }
+
+        public sealed class GenerateAsyncMethod : BaseGeneratorTests.BaseGenerateAsyncMethod<CrudControllerLoggerMessageActionGenerator, CrudControllerLoggerMessageActionFeatureFlags, CrudControllerLoggerMessageActionGeneratorProcessor, IEntityGenerationModel>
+        {
+            public GenerateAsyncMethod(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            protected override Func<AttributeData, CrudControllerLoggerMessageActionGenerator> GetFactory()
+            {
+                return data => new TestCrudControllerLoggerMessageActionGenerator();
+            }
+        }
+    }
+}
