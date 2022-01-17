c50b9eaafe3dbf0cff40f613d8fbb085fa3d9464
David Vreony <dpvreony@users.noreply.github.com>
2022-01-17 22:35:02 +0000

    add more unit tests

--- src/Dhgms.Nucleotide.UnitTests/Generators/Features/ReactiveUI/WPF/ReactiveMetroWindowClassGeneratorTests.cs
+++ src/Dhgms.Nucleotide.UnitTests/Generators/Features/ReactiveUI/WPF/ReactiveMetroWindowClassGeneratorTests.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using Dhgms.Nucleotide.Generators.Features.ReactiveUI.Wpf;
+using Dhgms.Nucleotide.SampleGenerator;
+using Microsoft.CodeAnalysis;
+using Xunit.Abstractions;
 
 namespace Dhgms.Nucleotide.UnitTests.Generators.Features.ReactiveUI.WPF
 {
+    [ExcludeFromCodeCoverage]
     public static class ReactiveMetroWindowClassGeneratorTests
     {
+        public sealed class ConstructorMethod : BaseGeneratorTests.BaseConstructorMethod<ReactiveMetroWindowClassGenerator, ReactiveWindowClassFeatureFlags, ReactiveMetroWindowClassGeneratorProcessor, ReactiveWindowGenerationModel>
+        {
+            public ConstructorMethod(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            protected override Func<AttributeData, ReactiveMetroWindowClassGenerator> GetFactory()
+            {
+                return data => new TestReactiveMetroWindowClassGenerator();
+            }
+        }
+
+        public sealed class GenerateAsyncMethod : BaseGeneratorTests.BaseGenerateAsyncMethod<ReactiveMetroWindowClassGenerator, ReactiveWindowClassFeatureFlags, ReactiveMetroWindowClassGeneratorProcessor, ReactiveWindowGenerationModel>
+        {
+            public GenerateAsyncMethod(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            protected override Func<AttributeData, ReactiveMetroWindowClassGenerator> GetFactory()
+            {
+                return data => new TestReactiveMetroWindowClassGenerator();
+            }
+        }
     }
 }
