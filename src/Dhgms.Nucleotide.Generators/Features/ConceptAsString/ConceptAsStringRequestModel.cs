5a0afbf6805deab6e6d8f6894ef28baf4605e528
David Vreony <dpvreony@users.noreply.github.com>
2024-06-12 21:45:42 +0100

    Update ConceptAsStringRequestModel.cs

--- src/Dhgms.Nucleotide.Generators/Features/ConceptAsString/ConceptAsStringRequestModel.cs
+++ src/Dhgms.Nucleotide.Generators/Features/ConceptAsString/ConceptAsStringRequestModel.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Dhgms.Nucleotide.Generators.Features.ConceptAsString
 {
-    internal class ConceptAsStringRequestModel
-    {
-    }
+    /// <summary>
+    /// Represents the request model for the Concept As String feature.
+    /// </summary>
+    /// <param name="MappingDictionary">A dictionary of namespaces and classes that are a concept that can be represented as a string.</param>
+    public sealed record ConceptAsStringRequestModel(IReadOnlyDictionary<string, IReadOnlyCollection<string>> MappingDictionary);
 }
