306e74f6f4eba1db3f6485957d18032c2684dea3
David Vreony <dpvreony@users.noreply.github.com>
2026-01-27 17:24:00 +0000

    add int test project

---
+++ src/Dhgms.Nucleotide.IntegrationTests/SampleGenerator/FakeTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Dhgms.Nucleotide.IntegrationTests.SampleGenerator
+{
+    /// <summary>
+    /// Fake test to ensure test project builds/runs.
+    /// </summary>
+    public sealed class FakeTest
+    {
+        /// <summary>
+        /// fake test method.
+        /// </summary>
+        [Fact]
+        public void FakeTestMethod()
+        {
+            // Fake test to ensure test project builds/runs.
+            // need to do some housekeeping and move the actual integration tests out of unit tests project.
+            Assert.True(true);
+        }
+    }
+}
